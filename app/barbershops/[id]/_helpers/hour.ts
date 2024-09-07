9bd678d9993e48cfeb745d4f6d58958d6700bda8
Cícero Santos <cicero.dsantos@outlook.com.br>
2024-09-07 19:02:26 -0300

    feature: added option name user booking

--- app/barbershops/[id]/_helpers/hour.ts
+++ app/barbershops/[id]/_helpers/hour.ts
@@ -3,27 +3,27 @@ import { addMinutes, setHours, setMinutes, isAfter, format } from 'date-fns';
 export function generateDayTimeList(date: Date, userId: String): string[] {
     const currentTime = setMinutes(new Date(), 0); // Arredonda para a hora completa atual
     const startTime = setMinutes(setHours(date, 9), 0); // horário inicial 9:00
-    const endTime = setMinutes(setHours(date, 19), 0); // horário final 17:00
+    const endTime = setMinutes(setHours(date, 20), 0); // horário final 17:00
     const interval = 60; // intervalo de 60 minutos
     const timeList: string[] = []; // lista de horários disponíveis
-    
-    const blockedTimesP = ["12:00", "13:00"]; 
-    const blockedTimesL = ["13:00", "14:00"]; 
-    
-    
-
     let nextAvailableTime = startTime;
-
+    
+    const blockedTimesP = ["11:00", "12:00"]; 
+    const blockedTimesL = ["12:00", "13:00"]; 
+    
+    const blockedTimesMap: { [key: string]: string[] } = {
+        "6b32011b-d357-499b-ad4d-cf742ce4072e": blockedTimesP, // Pedro
+        "67f2bf67-28f9-4685-99fe-624c637efc45": blockedTimesL, // Leo
+      };
+    
     while (nextAvailableTime < endTime) {
         if (isAfter(nextAvailableTime, currentTime)) {
             let formattedTime = format(nextAvailableTime, 'HH:mm');
-            if ((userId === "6b32011b-d357-499b-ad4d-cf742ce4072e") && !blockedTimesP.includes(formattedTime)) { //pedro
-                timeList.push(formattedTime);  
-            }else if (userId == "67f2bf67-28f9-4685-99fe-624c637efc45" && !blockedTimesL.includes(formattedTime)) { //leo
-                timeList.push(formattedTime);  
-            }else{
-                timeList.push(formattedTime);  
-            }
+            
+            if (!blockedTimesMap[userId as any]?.includes(formattedTime)) {
+                timeList.push(formattedTime);
+              }
+
             console.log(userId)
             
         }
