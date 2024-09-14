ea05c53757e21f211815c65f646692c177d76c1d
Cícero Santos <cicero.dsantos@outlook.com.br>
2024-09-14 16:20:29 -0300

    feature: option hours in saturday

--- app/barbershops/[id]/_helpers/hour.ts
+++ app/barbershops/[id]/_helpers/hour.ts
@@ -10,10 +10,8 @@ export function generateDayTimeList(date: Date, userId: String): string[] {
     const timeList: string[] = []; // lista de horários disponíveis
     let nextAvailableTime = startTime;
     
-    
     const blockedTimesP = ["11:00", "12:00"]; 
     const blockedTimesL = ["12:00", "13:00"]; 
-    const blockedTimesSaturday = ["12:00", "13:00"]; 
     
     const blockedTimesMap: { [key: string]: string[] } = {
         "6b32011b-d357-499b-ad4d-cf742ce4072e": blockedTimesP, // Pedro
